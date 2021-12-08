7cef9a41c8b77891c81753d24f4bdb660dbca908
Oskar Steiger <okr765@gmail.com>
2021-12-07 20:46:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { Stack, TextField, Typography, Box } from "@mui/material";
+import React from "react";
+import table from "./perfectTravel.json";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { output: "" };
+
+    this.onAngleChange = this.onAngleChange.bind(this);
+  }
+
+  onAngleChange(e) {
+    let val = e.target.value;
+    val = parseFloat(val);
+    while (val > 180) {
+      val -= 360;
+    }
+    while (val < -180) {
+      val += 360;
+    }
+    this.setState({ output: table[val.toString()] || "" });
+  }
+
+  render() {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        minWidth="100vw"
+        position="fixed"
+        left={0}
+        top={0}
+      >
+        <Stack spacing={1}>
+          <TextField
+            variant="filled"
+            label="Angle"
+            onChange={this.onAngleChange}
+          />
+          <Typography textAlign="center" variant="h5">{this.state.output}</Typography>
+        </Stack>
+      </Box>
+    );
+  }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
